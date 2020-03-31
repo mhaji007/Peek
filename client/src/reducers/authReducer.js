b7e3b00a329b464cad2967c24de54acf5c27bb4c
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-03-31 14:15:55 -0400

    Refactor action types

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import {SIGN_IN, SIGN_OUT} from '../actions/types';
+
 const INITIAL_STATE = {
     isSignedIn: null
 
@@ -5,9 +7,9 @@ const INITIAL_STATE = {
 
 export default (state=INITIAL_STATE, action) => {
     switch(action.type) {
-        case 'SIGN_IN':
+        case SIGN_IN:
             return {...state, isSignedIn:true};
-        case 'SIGN_OUT':
+        case SIGN_OUT:
             return {...state, isSignedIn:false};
         default:
         return state;
