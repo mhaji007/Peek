1aeee583812790dd13005cd18945345c91b860bd
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-03-31 18:10:34 -0400

    Handle events

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 
 class StreamCreate extends React.Component {
+renderInput(formProps){
+    // return <input onChange={formProps.input.onChange} value={formProps.input.value} />
+    return <input {...formProps.input} />
+    }
  render () {
     return(
         <form>
-            <Field name="title" />
-            <Field name="description" />
+            <Field name="title" component={this.renderInput} />
+            <Field name="description" component={this.renderInput} />
         </form>
     ) 
  }
