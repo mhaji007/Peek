0972ad2760af7fa9a51d54a330c49516aa7b3530
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-01 16:21:42 -0400

    Handle form submission

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -11,11 +11,16 @@ renderInput(formProps){
         </div>
     );
     }
+
+    onSubmit(formValues) {
+      
+    }
  render () {
     return(
-        <form className="ui form">
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
             <Field name="title" component={this.renderInput} label = "Enter Title" />
             <Field name="description" component={this.renderInput} label = "Enter Description" />
+        <button className="ui button primary">Submit</button>
         </form>
     ) 
  }
