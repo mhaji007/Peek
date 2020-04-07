b9d6b6583858fd94a5d5c8a10e0711fcf4a0f218
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-07 19:02:20 -0400

    Refactor StreamForm to avoid changes to properties

--- client/src/components/streams/StreamEdit.jsx
+++ client/src/components/streams/StreamEdit.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import {connect} from 'react-redux';
 import {fetchStream, editStream} from '../../actions';
@@ -21,7 +22,7 @@ class StreamEdit extends React.Component {
         return (
             <div>
                 <h3>Edit a Stream</h3>
-                <StreamForm initialValues={this.props.stream} onSubmit={this.onSubmit} />
+                <StreamForm initialValues={_.pick(this.props.stream,'title','description')} onSubmit={this.onSubmit} />
             </div>
         )
     }
