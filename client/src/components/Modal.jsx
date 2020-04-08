b52097a019aec62e37d1260ae212f4ee86afae25
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-08 16:51:59 -0400

    Stop event propogation to allow modal hiding by outside click only

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -5,7 +5,7 @@ import history from '../history';
 const Modal = props => {
     return ReactDOM.createPortal(
         <div onClick={()=>history.push('/')} className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
+            <div onClick={e=>e.stopPropagation()} className="ui standard modal visible active">
                 <div className="header">Delete Stream </div>
                 <div className="content">Are you sure you want to delete the stream?</div>
                 <div className="actions">
