3eccf2a71d97adb8c55c118ad36931ef68d06aff
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-09 14:11:48 -0400

    Refactor StreamDelete with fragments

--- client/src/components/streams/StreamDelete.jsx
+++ client/src/components/streams/StreamDelete.jsx
@@ -3,11 +3,11 @@ import Modal from '../Modal';
 
 const StreamDelete = () => {
     const actions = (
-        <div>
+        <React.Fragment>
         <button className="ui button negative">Delete</button>
        
         <button className="ui button">Cancel</button>
-        </div>
+        </React.Fragment>
     );
     return (
         <div>
